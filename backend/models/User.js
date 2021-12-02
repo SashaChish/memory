ae2ccc701ab5696e9affccb10418dc35069843a4
Serhiy Dvoryannikov <dvoryannikovserhiy@gmail.com>
2021-12-02 21:03:47 +0200

    setup basic structure, connection between bd and server, create api for registration and authentication

---
+++ backend/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    fullName: {
+        type: String, 
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type:String,
+        required: true
+    },
+    avatar: {
+        type:String,
+        default: 'https://nogivruki.ua/wp-content/uploads/2018/08/default-user-image.png'
+    }
+});
+//збережені пости, пости, підписки, лайки, коменти, date
+module.exports = User = mongoose.model("user", UserSchema);
