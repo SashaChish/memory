dffef1f4a7727ab3d1db05935bd21737d3a7b092
SashaChish <sasha.chishen@gmail.com>
2021-12-04 17:55:19 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { RegistrationPage } from './pages';
-
-const Login = () => {
-	return (
-		<div>
-			<Link to='/registration'>no have account?</Link>
-		</div>
-	);
-};
 
 export default function App() {
 	return (
 		<Router>
 			<Routes>
-				<Route path='login' element={<Login />} />
 				<Route path='registration' element={<RegistrationPage />} />
-				<Route path='home' element={<div>after register</div>} />
 			</Routes>
 		</Router>
 	);
