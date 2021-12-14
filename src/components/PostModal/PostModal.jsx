c1adba9de6546572d8095f3cc9f1beb11c745d1a
Serhiy Dvoryannikov <dvoryannikovserhiy@gmail.com>
2021-12-14 23:12:27 +0200

    not finished popup

--- src/components/PostModal/PostModal.jsx
+++ src/components/PostModal/PostModal.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'react-moment';
 import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
@@ -9,17 +10,27 @@ import DialogActions from '@mui/material/DialogActions';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+
+import { Link } from 'react-router-dom';
+
+import {
+	DialogInfo,
+	PostImg,
+	PostUsername,
+	DialogComment,
+	CommentDate,
+	CommentForm,
+	PostIcon,
+	PostSaved,
+} from './PostModal.style';
 
 import { $api } from '../../http';
-
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-	'& .MuiDialogContent-root': {
-		padding: theme.spacing(2),
-	},
-	'& .MuiDialogActions-root': {
-		padding: theme.spacing(1),
-	},
-}));
 
 const BootstrapDialogTitle = (props) => {
 	const { children, onClose, ...other } = props;
@@ -47,8 +58,12 @@ const BootstrapDialogTitle = (props) => {
 
 export const PostModal = ({ modalControl, postId }) => {
 	const [postInfo, setPostInfo] = useState({});
+	const [commentText, setCommentText] = useState('');
 
 	useEffect(() => {
+		setPostInfo({});
+		setCommentText('');
+
 		const fetchData = async () => {
 			const result = await $api.get(`/posts/${postId}`);
 			setPostInfo(result.data);
@@ -58,40 +73,138 @@ export const PostModal = ({ modalControl, postId }) => {
 		postId !== '' && fetchData();
 	}, [postId]);
 
+	const handleSendComment = async (e) => {
+		e.preventDefault();
+
+		try {
+			const result = await $api.post(`/posts/comment/${postId}`, {
+				text: commentText,
+			});
+
+			setPostInfo({ ...postInfo, comments: result.data });
+
+			setCommentText('');
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
 	return (
-		<BootstrapDialog
+		<Dialog
 			onClose={() => modalControl.handleCloseModal()}
-			aria-labelledby='customized-dialog-title'
+			aria-labelledby='post-popup'
 			open={modalControl.open}
+			PaperProps={{
+				style: {
+					borderRadius: '0 5px 5px 0',
+					display: 'flex',
+					flexDirection: 'row',
+					width: '90vw',
+					maxWidth: '100vw',
+					minHeight: '90vh',
+				},
+			}}
 		>
-			<BootstrapDialogTitle
-				id='customized-dialog-title'
-				onClose={() => modalControl.handleCloseModal()}
-			>
-				Modal title
-			</BootstrapDialogTitle>
-			<DialogContent dividers>
-				<Typography gutterBottom>
-					Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-					dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-					consectetur ac, vestibulum at eros.
-				</Typography>
-				<Typography gutterBottom>
-					Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-					Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-				</Typography>
-				<Typography gutterBottom>
-					Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
-					magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
-					ullamcorper nulla non metus auctor fringilla.
-				</Typography>
-			</DialogContent>
-			<DialogActions>
-				<Button autoFocus onClick={() => modalControl.handleCloseModal()}>
-					Save changes
-				</Button>
-			</DialogActions>
-		</BootstrapDialog>
+			<PostImg
+				src={postInfo?.picture}
+				alt={`${postInfo?.username}'s picture`}
+			/>
+
+			<DialogInfo>
+				<BootstrapDialogTitle
+					id='customized-dialog-title'
+					onClose={() => modalControl.handleCloseModal()}
+					sx={{ display: 'flex', alignItems: 'center' }}
+				>
+					<Link to={`/${postInfo?.username}`}>
+						<Avatar
+							sx={{ width: 40, height: 40, marginRight: '20px' }}
+							alt={postInfo?.username}
+							src={postInfo?.avatar}
+						/>
+					</Link>
+					<Link
+						to={`/${postInfo?.username}`}
+						style={{ textDecoration: 'none' }}
+					>
+						<PostUsername>{postInfo?.username}</PostUsername>
+					</Link>
+				</BootstrapDialogTitle>
+				<DialogContent
+					dividers
+					sx={{ height: 'calc(100% - 65px - 70px - 75px)' }}
+				>
+					{postInfo?.comments?.map((comment) => (
+						<DialogComment>
+							<Link
+								to={`/${comment.username}`}
+								style={{ textDecoration: 'none' }}
+							>
+								<Avatar
+									sx={{ width: 40, height: 40, marginRight: '20px' }}
+									alt={comment.username}
+									src={comment.avatar}
+								/>
+							</Link>
+							<Typography
+								gutterBottom
+								sx={{ position: 'relative', width: '100%' }}
+							>
+								<Link
+									to={`/${comment.username}`}
+									style={{ textDecoration: 'none' }}
+								>
+									<PostUsername>{comment.username}</PostUsername>
+								</Link>
+								{'  '}
+								{comment.text}
+								<CommentDate>
+									<Moment fromNow>{new Date(comment.date)}</Moment>
+								</CommentDate>
+							</Typography>
+						</DialogComment>
+					))}
+				</DialogContent>
+				<DialogContent>
+					<PostIcon>
+						<FavoriteBorderIcon />
+					</PostIcon>
+					<PostIcon>
+						<ChatBubbleOutlineIcon />
+					</PostIcon>
+					<PostSaved>
+						<BookmarkBorderIcon />
+					</PostSaved>
+				</DialogContent>
+				<DialogActions sx={{ height: '60px', borderTop: '1px solid #E0E0E0' }}>
+					<CommentForm onSubmit={(e) => handleSendComment(e)}>
+						<TextField
+							variant='standard'
+							margin='normal'
+							fullWidth
+							multiline
+							name='comment'
+							autoFocus
+							placeholder='Write a comment...'
+							style={{ marginLeft: '20px' }}
+							value={commentText}
+							onChange={(e) => setCommentText(e.target.value)}
+							InputProps={{
+								disableUnderline: true,
+							}}
+						/>
+						<Button
+							variant='text'
+							type='submit'
+							disabled={commentText.trim() === ''}
+							style={{ backgroundColor: 'transparent' }}
+						>
+							Publish
+						</Button>
+					</CommentForm>
+				</DialogActions>
+			</DialogInfo>
+		</Dialog>
 	);
 };
 
